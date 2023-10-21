3ac1ef8d81dd8ebec2d38d1cf99d0449c75d2fd6
jdsam <jdsam@Rezeal.jdsam.virtualbox.org>
2023-10-21 14:14:43 +0800

    update Readme, add two function

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # learn_python
 python learning and playground
+user_login.py
+    a user login system
+    two functions: id_reg-> register username and password
+                   id_login-> login id
--- user_login.py
+++ user_login.py
@@ -1,23 +1,40 @@
 print('This is test page')
 
+def id_reg():
+    print("Creating user id ......")
+    id_name=input("Please enter user name: ")
+    id_pass=input("Please enter password: ")
 
-my_id={'name'    :'jdsam',
-       'pass_id' :'abc123'
-       }
-count = 0
-#print(my_id)
-#print(my_id['name'],my_id['pass_id'])
+    my_id={'name'    :id_name,
+           'pass_id' :id_pass
+           }
+    print("Register successful !")
+    return my_id
 
+def login_id(my_id):
+    count = 0
+
+    while True:
+        username=input("Please enter username: ")
+        password = input("Please enter password: ")
+        if username == my_id['name'] and password == my_id['pass_id']:
+            print(f"Welcome, {username} ^ ^")
+            break
+        elif count==2:
+            print(f"Exceed number of tried, please contact support!!")
+            break
+        else:
+            count +=1
+            print(f"Wrong username or password, please try again!")
+num_ppl=0
 while True:
-    username=input("Please enter username: ")
-    password = input("Please enter password: ")
-    if username == my_id['name'] and password == my_id['pass_id']:
-        print(f"Welcome, {username} ^ ^")
-        break
-    elif count==2:
-        print(f"Exceed number of tried, please contact support!!")
+
+    if num_ppl == 5 :
+        print("Database is FULL!!")
         break
     else:
-        count +=1
-        print(f"Wrong username or password, please try again!")
+        id_dict = id_reg()
+        num_ppl +=1
+        print(f"user number: {num_ppl}")
 
+    login_id(id_dict)
